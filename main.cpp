a4f4354fb590b7f38262dad9599d7e76a074d880
Kayvon Washington <washingtonkayvon@aol.com>
2025-02-12 10:43:48 -0800

    Inventory System - Struct
    Implemented an inventory system for any player. Declare inventory name (user, using the inventory), and then add an Item based on, Weapon Type, Name of the weapon, and Quantity.
    
    Ex. 
    Inventory player1;
    player1.addItem("Potions", "Health Elixir", "3");
    
    This will store 3 Health Elixirs inside the Potions list item.

--- main.cpp
+++ main.cpp
@@ -5,9 +5,67 @@
 #include <cstring>
 #include <sys/wait.h>
 
+//Inventory 
+#include <map>
+#include <list>
+#include <string>
+#include <algorithm>
+
 using namespace std;
 
 #define MAX_LINE 80 // The maximum length command
+
+//Inventory System - Struct 
+class Inventory {
+  private: 
+      //Map (Category Name) to a Map (item Name, and quantity)
+      //Map -> Map
+      map<string, map<string, int>> itemsByCategory;
+
+      //Empty list
+      list<string> categoryOrder;
+
+  public:
+      void addItem(const string& category, const string& item, int quantity = 1){
+        //if the category is not found in the list | Prevent Duplicate categories
+        if(find(categoryOrder.begin(), categoryOrder.end(), category) == categoryOrder.end()){
+          //Add the category to the list
+          categoryOrder.push_back(category);
+        }
+        //Add item to specific category
+        itemsByCategory[category][item] += quantity;
+      }
+
+      void displayInventory(){
+        //No items within Inventory
+        if(itemsByCategory.empty()){
+          cout << "Your inventory is empty.\n";
+          return;
+        }
+
+        //Predefined Category order
+        const string categoryOrder[] = {"Weapons", "Potions", "Artifacts"};
+
+        cout << "\n===== Inventory =====\n";
+        
+        //Display by category
+        //For each category name within the category order
+        for(const auto& categoryName : categoryOrder) {
+          auto it = itemsByCategory.find(categoryName);
+
+          //If a Category Name within categoryOrder is found
+          if(it != itemsByCategory.end()){
+            cout << "\n[ " << categoryName << " ]\n";
+
+            //For each item within the category, print | Pointint to value
+            for(const auto& item : it->second){
+              cout << " " << item.first << " x" << item.second << "\n";
+            }
+          }
+        }
+        cout << "\n====================\n";
+      }
+};
 
 int parse_command(char command[], char *args[]) {
     int i = 0;
