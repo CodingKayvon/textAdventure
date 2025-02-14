7f5aac4c0398828f6064c03b661c59fc463a6459
Zach Miller <zmiller7108@gmail.com>
2025-02-13 18:35:49 -0800

    Create activities.h

---
+++ activities.h
@@ -0,0 +1,14 @@
+class Activities {
+    
+
+    private:
+    // string checkItem(bool hasItem);
+
+    public:
+    /**
+     * riddle function to create the instance of the event
+     * @return 0 (for now, may have it return specific int for rewards later)
+     */
+    void riddleGame(bool requirment);
+
+};
